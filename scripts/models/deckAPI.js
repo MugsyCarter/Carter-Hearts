76a2eb96be9ebafa497ac85204a08b92fbaa8f73
Mugsy Carter <mugsycarter@gmail.com>
2016-12-05 14:02:25 -0800

    passing almost complete.

--- scripts/models/deckAPI.js
+++ scripts/models/deckAPI.js
@@ -4,7 +4,12 @@
   Deck.shuffled = [];
   Deck.compDecks =[];
   //sorts cards by number
-
+  Deck.passObject = {
+    user: [],
+    1: [],
+    2: [],
+    3: [],
+  };
   Deck.sortCards = function(hand){
     hand.sort(function(a,b){
       return a.value-b.value;
@@ -61,6 +66,7 @@
 
 //ajax call fetches a shuffled deck and deals out 13 cards
   Deck.dealCards = function(){
+    Turn.turn ++;
     console.log('dealing cards');
     $.ajax('https://deckofcardsapi.com/api/deck/new/draw/?count=52', {
       method: 'GET',
@@ -137,46 +143,54 @@
       $('.comp1Card').hide();
       $('.comp2Card').hide();
       $('.comp3Card').hide();
+
+//increment the turn counter
+//if not a hold hand begin passing phase
+      if (Turn.pass !== 'none'){
+
 //show pass button once dealt and sorted
-      $('#pass-button').fadeIn();
-      Turn.newTurn();
-
-
-
+        $('#pass-button').fadeIn();
+     
 //Allow users to select pass-button
-      Deck.passArray = [];
-      $('.card-img').on('click', function(event){
-        event.preventDefault();
-        console.log('even target is ', event.target);
-        var clickParent = $(event.target).parent()[0];
-        console.log('event parent is ', clickParent);
-        console.log('this is ', this);
-        console.log('event parent ID is ', clickParent.id);
+        Deck.passArray = [];
+        $('.card-img').on('click', function(event){
+          event.preventDefault();
+          console.log('even target is ', event.target);
+          var clickParent = $(event.target).parent()[0];
+          console.log('event parent is ', clickParent);
+          console.log('this is ', this);
+          console.log('event parent ID is ', clickParent.id);
         
 
-        var index = Deck.passArray.indexOf(clickParent);
-        console.log(index);
-        if(index===-1){
-          Deck.passArray.push(clickParent);
-          console.log('click parent is ', clickParent);
-          $(this).css({'position':'relative','bottom':'20px'});
-          console.log(Deck.passArray);
-          if (Deck.passArray.length > 3){
-            console.log('time to remove', Deck.passArray[0]);
-            $(Deck.passArray[0].firstChild).css({'position':'relative','bottom':'0px'});
-            Deck.passArray.shift();
-            console.log('removed card.  Pass Array is ', Deck.passArray);
+          var index = Deck.passArray.indexOf(clickParent);
+          console.log(index);
+          if(index===-1){
+            Deck.passArray.push(clickParent);
+            console.log('click parent is ', clickParent);
+            $(this).css({'position':'relative','bottom':'20px'});
+            console.log(Deck.passArray);
+            if (Deck.passArray.length > 3){
+              console.log('time to remove', Deck.passArray[0]);
+              $(Deck.passArray[0].firstChild).css({'position':'relative','bottom':'0px'});
+              Deck.passArray.shift();
+              console.log('removed card.  Pass Array is ', Deck.passArray);
+            }
           }
-        }
-        else{
-          console.log('index is', index);
-          Deck.passArray.splice(index,1);
-          console.log('card being removed.', this);
-          $(this).css({'position':'relative','bottom':'0px'});
-          console.log(Deck.passArray);
-        }
-      });
-
+          else{
+            console.log('index is', index);
+            Deck.passArray.splice(index,1);
+            console.log('card being removed.', this);
+            $(this).css({'position':'relative','bottom':'0px'});
+            console.log(Deck.passArray);
+          }
+        });
+      }
+      //else its a hold hand so begin play
+      else{
+        $('#hold-hand-message').fadeIn().fadeOut(2000);
+        $('#begin-play-message').fadeIn().fadeOut(2000);
+        //put the start play function call here once written
+      }
 
 
 
